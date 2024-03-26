6e2ea93e690ca8a1a7b8906459be90524d14498d
Karthik Chitturi <karthik_csd@srkrec.edu.in>
2024-03-26 10:19:18 +0530

    commit8

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,10 @@ import './App.css'
 function App() {
  return (
     <div className="App">
+      <h2 className='text-center text-2xl text-white mt-4 font-bold'>IISc Banglore Web Development Internship Task01</h2>
+      <p className='text-center text-xl text-white my-3'>Name : <a href='https://ch-gold.vercel.app' target='new' className='text-blue-400 underline'>Karthik Chitturi</a></p>
       <Form/>
+      <p className='text-center text-yellow-500 text-m'>Employed YOLOv8 and OpenCV to implement object-detection.</p>
     </div>
  );
 }
--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -27,7 +27,7 @@ const Form = () => {
   return (
     <>
       <form className="max-w-lg mx-auto">
-        <label className="block mb-2 text-xl font-medium text-gray-900 dark:text-white" htmlFor="user_avatar">Upload file</label>
+      <div className="my-2 text-sm text-gray-500 dark:text-gray-300" id="user_avatar_help">Upload a jpg image to get started!</div>
         <input
           className="block w-full text-sm text-gray-900 border border-gray-500 rounded-lg cursor-pointer bg-gray-300 dark:text-gray-400 focus:outline-none dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 p-5"
           aria-describedby="user_avatar_help"
@@ -38,7 +38,6 @@ const Form = () => {
             handleFileChange(e);
           }}
         />
-        <div className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="user_avatar_help">Upload a traffic image for further processing.</div>
       </form>
       <div className="flex p-2">
         {uploadedImage && (
